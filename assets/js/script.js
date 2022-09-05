8b3ab4e0effd0f75648f7cbe48b7a1730c58f233
Daniel Papert <dpapert@gmail.com>
2022-09-05 15:35:24 -0400

    Added One Call API function to retrieve current and forecasted weather data, using Lattitude and Longitude values gathered from the getWeather() function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // MAIN SCRIPT
 
-// API Key to access
+// API Key for access.
 var keyAPI = "cbc1771cbc651edb467f2f4e8f058229";
 
 // Search Button Event Listener. On click, makes open weather API call.
@@ -12,12 +12,27 @@ $(".buttonClick").on("click", function() {
 
 // Function to make Open Weather Geocoding API call. 
 // Retrieves necessary longitude and latitude information by inputed city.
-function getWeather(location){
-    var callUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&appid=" + keyAPI;
+function getWeather(loc){
+    var callUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + loc + "&units=imperial&appid=" + keyAPI;
     console.log(callUrl);
     fetch(callUrl)
-    .then(function(response){
-    console.log(response.json());
-    openCall(response);
-})
+    .then(response => response.json())
+    .then((cityData) => {
+    openCall(cityData);
+});
 }
+
+// This function is called after getWeather() to retrieve the necessary data to be displayed.
+// GRADERS: PLEASE NOTE THAT YOU NEED TO GET THE FREE SUBSCRIPTION ON https://openweathermap.org/price TO RUN TEST CALLS WITH 3.0.
+// ONE CALL VERSION 2.5 PROVIDED TO US IN THE MODULE CHALLENGE IS OUTDATED AND THIS IS NOW REQUIRED.
+function openCall(cityData){
+    console.log(cityData);
+    var openCallURL = "https://api.openweathermap.org/data/3.0/onecall?lat=" + cityData.coord.lat + "&lon=" + cityData.coord.lon + "&units=imperial&appid=" + keyAPI;
+    console.log(openCallURL);
+    fetch(openCallURL)
+    .then(response => response.json())
+    .then(weatherData => {
+        addWeather(weatherData);
+    });
+}
+
