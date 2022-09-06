ebf50a4394d523eff3913a2d7f57b0cc58ff9eb8
Daniel Papert <dpapert@gmail.com>
2022-09-06 02:43:02 -0400

    Added clear search functionality. Added search history button click functionality. Added dynamic UV value display that changes color depending on sevrity. Added footer. Adjusted styles. Added content to README.md file.

---
+++ README.md
@@ -0,0 +1,33 @@
+# Work Day Scheduler Starter Code
+
+## Purpose
+This application allows you to search both current weather conditions
+and five-day forecasts by city. Upon searching, the application provides
+the user with the following information:
+- Temperature (in degrees Fahrenheit)
+- Humidity 
+- Wind Speed (in MPH)
+- UV Index (for current conditions only).
+
+The application stores the search history such that users can click on recent history
+to update the search. Users may also clear their history with a delete button.
+
+If a user enters a search already in their search history, that location will move to the 
+top of the search history and will not repeat.
+
+## Built With
+* HTML (with bootstrap)
+* CSS (With external stylesheets, including FontAwesome).
+* Javascript (With external scripts such as moment.js)
+* JQuery 
+* Open Weather API (https://openweathermap.org/api)
+
+## Screenshots
+![](/assets/images/screenshot1.PNG)
+
+## Website
+https://dpapert1995.github.io/weather-dashboard-dp/
+
+## Contribution
+Authors: Daniel Papert
+© 2022
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,38 @@
 
 // API Key for access.
 var keyAPI = "cbc1771cbc651edb467f2f4e8f058229";
-var day = moment().format('LLLL');
+
+// Display search history as clickable butons
+var storedSearches = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+// Displays search history on page.
+displayHistory(storedSearches);
+
+// Displays current weather of most recent search if one exists. 
+// If Search History is empty, nothing happens!
+    getWeather(storedSearches[storedSearches.length-1]);
 
 // Search Button Event Listener. On click, makes open weather API call.
 $(".buttonClick").on("click", function() {
     var location = $(this).siblings(".input").val();
-
     $(this).siblings(".input").val("");
     getWeather(location);
+    displayHistory(storedSearches);
+});
+
+// Delete Button Event Listener. On click, delete.
+$(".deleteClick").on("click", function() {
+    localStorage.removeItem("searchHistory")
+    storedSearches = [];
+    $("#search-history").empty();
+});
+
+// Search History Button Event Listener. On click, search for that specific city.
+$("#search-history").on("click", function() {
+    var location = event.target.id;
+    console.log(location);
+    getWeather(location);
+    displayHistory(storedSearches);
 });
 
 // Function to make Open Weather Geocoding API call. 
@@ -32,6 +56,8 @@ function openCall(cityData){
     .then(response => response.json())
     .then(weatherData => {
         addWeather(cityData.name, weatherData);
+        // Only adds to history if call is successful to this point.
+        addToHistory(cityData.name);
     });
 }
 
@@ -50,6 +76,10 @@ function addWeather(city, weatherData) {
     $("#uvIndex").empty();
     $("#forecast-display").empty();
 
+    // Day and Time From moment.js
+    // Nested in this function so it updates each time a Weather Call is made, rather than a page refresh.
+    var day = moment().format('LLLL');
+
     // Populate and display today's weather info per the user requirements.
     $("#todays-info").removeClass("hide");
     $("#todays-info").addClass("show");
@@ -58,7 +88,28 @@ function addWeather(city, weatherData) {
     $("#temperature").append("Temperature: " + weatherData.current.temp + " °F");
     $("#humidity").append("Humidity: " + weatherData.current.humidity + "%");
     $("#windSpeed").append("Wind Speed: " + weatherData.current.wind_speed + " MPH");
-    $("#uvIndex").append("UV Index: " + weatherData.current.uvi);
+    $("#uvIndex").append("UV Index: " + `<span id = uvNum> ${weatherData.current.uvi}</span>`);
+
+    // Determines background color for UV Index depending on sevrity:
+    let uv = weatherData.current.uvi;
+    if (uv < 3) {
+        $("#uvNum").css("background-color", "green");
+        $("#uvNum").css("color", "white");
+    }
+    else if (3 <= uv && uv < 6) {
+        $("#uvNum").css("background-color", "yellow");
+    }
+    else if (6 <= uv && uv < 8) {
+        $("#uvNum").css("background-color", "orange");
+    }
+    else if (8 <= uv && uv < 10) {
+        $("#uvNum").css("background-color", "red");
+        $("#uvNum").css("color", "white");
+    }
+    else {
+        $("#uvNum").css("background-color", "purple");
+        $("#uvNum").css("color", "white");
+    }
 
     // Populate and display five day weather forecast info per the user requirements.
     $("#five-day-forecast").removeClass("hide");
@@ -78,4 +129,30 @@ function addWeather(city, weatherData) {
              </div>`
         $("#forecast-display").append(fiveDayCard);
       }
+}
+
+// Function to add the search to history.
+// Conditions ensure no search is repeated in the side bar.
+function addToHistory (location) {
+    console.log(location);
+    // If the search matches any previous ones in local storage, move that search to the end of the array and reorder the other elements in the array.
+    if (storedSearches.includes(location)){
+        storedSearches.splice(storedSearches.indexOf(location),1);
+        storedSearches.push(location);
+    }
+    // Otherwise, put it at the end of the array.
+    else {
+        storedSearches.push(location);
+    }
+    localStorage.setItem("searchHistory", JSON.stringify(storedSearches));
+    console.log(localStorage.getItem("searchHistory"));
+    displayHistory(storedSearches);
+}
+
+// Function to Display Searches
+function displayHistory (storedSearches) {
+$("#search-history").empty();
+for (i = storedSearches.length-1; i > -1; i--){
+      $("#search-history").append(`<button id = ${storedSearches[i]} class= "searchClick" type = "submit"">${storedSearches[i]}</button>`)
+    }
 }
